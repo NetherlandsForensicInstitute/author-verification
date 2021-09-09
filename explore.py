ef131ce4639c1efd9fdfafa429f0bd52977143f5
Eleni Konstantina Sergidou <e.sergidou@nfi.nl>
2021-09-09 17:52:29 +0200

    extra adjustments

--- explore.py
+++ explore.py
@@ -11,6 +11,10 @@ import seaborn as sns
 from sklearn.metrics import roc_curve, confusion_matrix
 
 main_path = 'frida/predictions'
+runs = ['prep_gauss_2500']
+specific_run = runs[0]
+prediction_file = 'predictions_per_repeat_' + specific_run + '.json'
+param_file = 'param_' + specific_run + '.json'
 
 
 # for most freq words
@@ -48,6 +52,16 @@ def cllr(lrs, y):
     return lir.metrics.cllr(lrs, y)
 
 
+# recall (true positive rate)
+def recall(lrs, y):
+    return np.mean(lrs[y == 1] > 1)
+
+
+# precision
+def precision(lrs, y):
+    return np.mean(y[lrs > 1] == 1)
+
+
 # percentiles
 def percentiles_for_df(name, arr):
     log_arr = np.log10(arr)
@@ -58,7 +72,7 @@ def percentiles_for_df(name, arr):
 
 def lr_histogram(lrs, y, bins=20, title=''):
     """
-    plots the 10log lrs
+    plots the 10log lrs under the two hypotheses
     """
     log_lrs = np.log10(lrs)
 
@@ -72,6 +86,38 @@ def lr_histogram(lrs, y, bins=20, title=''):
     ax.set_title(title)
 
     return fig
+
+
+def accuracies_per_category(cols_to_group, lrs_col, y, df):
+    """
+    :param cols_to_group = list of column names
+    :param lrs_col = column name of the lrs
+    :param y = column name of the ground truth (binary 0/1)
+    :param df = dataframe with the mentioned columns
+
+    it returns a df where each comb of values in cols_to_group the counts and the accuracies for class 0 and 1
+    """
+    # counts
+    grouped_df = df.groupby(cols_to_group)[y].agg([lambda x: sum(x == 0), lambda x: sum(x)]).reset_index()
+    grouped_df.rename(columns={'<lambda_0>': "total_0", '<lambda_1>': "total_1"}, inplace=True)
+
+    # accuracies
+    df['pred'] = pd.Series(np.where((df[lrs_col] > 1) & (df[y] == 1), 1, np.where((df[lrs_col] <= 1) &
+                                                                                  (df[y] == 0), 0, 2)))
+    grouped_pred = df.groupby(cols_to_group)['pred'].agg([lambda x: sum(x < 2), lambda x: sum(x == 0),
+                                                          lambda x: sum(x == 1)]).reset_index()
+    new_col_name = lrs_col.replace('lrs_', '') + '_acc'
+    new_col_name_0 = lrs_col.replace('lrs_', '') + '_acc_0'
+    new_col_name_1 = lrs_col.replace('lrs_', '') + '_acc_1'
+    grouped_pred.rename(columns={'<lambda_0>': new_col_name, '<lambda_1>': new_col_name_0,
+                                 '<lambda_2>': new_col_name_1}, inplace=True)
+
+    grouped_df = grouped_df.merge(grouped_pred, on=cols_to_group)
+    grouped_df[new_col_name] = round(grouped_df[new_col_name] / (grouped_df['total_0'] + grouped_df['total_1']), 4)
+    grouped_df[new_col_name_0] = round(grouped_df[new_col_name_0] / grouped_df['total_0'], 4)
+    grouped_df[new_col_name_1] = round(grouped_df[new_col_name_1] / grouped_df['total_1'], 4)
+
+    return grouped_df.sort_values([new_col_name, new_col_name_1, new_col_name_0])
 
 
 if __name__ == '__main__':
@@ -136,17 +182,15 @@ if __name__ == '__main__':
         str.replace('Enligsh', 'English', regex=False)
 
     # create conversation metadata
-    conv_metadata = pd.DataFrame({'session': [2, 4, 6, 8], 'location': ['indoor', 'indoor', 'outdoor', 'indoor'],
+    conv_metadata = pd.DataFrame({'session': [2, 4, 6, 8], 'location': ['indoor', 'indoor', 'outdoor', 'outdoor'],
                                   'environment': ['quiet', 'noisy', 'quiet', 'noisy']})
 
-    # performance results (high level)
-    res_df = pd.DataFrame([['mfw', 0.516, 0.85, 0.154, 0.839, 0.587], ['voc', 0.139, 0.961, 0.037, 0.966, 0.858],
-                           ['multi', 0.1, 0.974, 0.027, 0.971, 0.907], ['combi', 0.116, 0.971, 0.032, 0.965, 0.897],
-                           ['featu', 0.115, 0.971, 0.028, 0.972, 0.894]],
-                          columns=['method', 'cllr', 'accuracy', 'eer', 'recall', 'precision'])
+    # parameters
+    with open(f'{main_path}/{param_file}') as p:
+        param = json.load(p)
 
     # predictions
-    with open(f'{main_path}/predictions_per_repeat_prep_gauss_2500.json') as f:
+    with open(f'{main_path}/{prediction_file}') as f:
         per_repeat = json.load(f)
 
     predictions = pd.DataFrame()
@@ -174,21 +218,31 @@ if __name__ == '__main__':
         predictions = predictions.append(temp_df, ignore_index=True)
         num_pairs = num_pairs.append(temp_num_df, ignore_index=True)
 
-    # performance results per repeat
+    # performance results on a high level and per repeat
+    res_df = pd.DataFrame(columns=['method', 'cllr', 'accuracy', 'eer', 'recall', 'precision'])
     metric_functions = {'cllr': cllr, 'acc': accuracy, 'eer': eer}
     metrics_per_repeat = pd.DataFrame()
 
     for col in ['lrs_mfw', 'lrs_voc', 'lrs_multi', 'lrs_comb', 'lrs_feat']:
 
+        method = col.replace('lrs_', '')
+
         # generate pav plots
         lir.plot_pav(predictions[col], predictions['y'], savefig=f'{main_path}/pav_{col}.png')
 
+        # cllr, accuracy, eer, recall and precision on a high level
+        res_df_row = pd.Series([method, cllr(predictions[col], predictions['y']), accuracy(predictions[col], predictions['y']),
+                      eer(predictions[col], predictions['y']), recall(predictions[col], predictions['y']),
+                      precision(predictions[col], predictions['y'])], index=res_df.columns)
+        res_df = res_df.append(res_df_row, ignore_index=True)
+
         # calculate cllr, accuracy and eer per repeat
         for m, f in metric_functions.items():
+
             temp = predictions.groupby('repeat')[['y', col]].apply(lambda x: f(x[col], x['y'])).to_frame()
             temp.reset_index(level=0, inplace=True)
             temp['metric'] = m
-            temp['method'] = col.replace('lrs_', '')
+            temp['method'] = method
             temp.rename(columns={0: 'value'}, inplace=True)
             temp = temp[['method', 'repeat', 'metric', 'value']]
             metrics_per_repeat = metrics_per_repeat.append(temp, ignore_index=True)
@@ -196,18 +250,13 @@ if __name__ == '__main__':
     # add info on predictions
     predictions['location_A'] = np.where(predictions['conv_A'].str.endswith(('2', '4')), 'indoor', 'outdoor')
     predictions['environment_A'] = np.where(predictions['conv_A'].str.endswith(('2', '6')), 'quiet', 'noisy')
-    predictions['location_A'] = predictions['location_A'] + '_' + predictions['environment_A']
     predictions['location_B'] = np.where(predictions['conv_B'].str.endswith(('2', '4')), 'indoor', 'outdoor')
     predictions['environment_B'] = np.where(predictions['conv_B'].str.endswith(('2', '6')), 'quiet', 'noisy')
-    predictions['location_B'] = predictions['location_B'] + '_' + predictions['environment_B']
     predictions = predictions.merge(conv_len.add_suffix('_A'), left_on='conv_A', right_on='conv_id_A')
     predictions = predictions.merge(conv_len.add_suffix('_B'), left_on='conv_B', right_on='conv_id_B')
     predictions = predictions.merge(spk_metadata.add_suffix('_A'), left_on='spk_A', right_on='spk_id_A')
     predictions = predictions.merge(spk_metadata.add_suffix('_B'), left_on='spk_B', right_on='spk_id_B')
-    predictions.drop(['environment_A', 'environment_B', 'spk_id_A', 'spk_id_B', 'conv_id_A', 'conv_id_B'], axis=1,
-                     inplace=True)
-
-
+    predictions.drop(['spk_id_A', 'spk_id_B', 'conv_id_A', 'conv_id_B'], axis=1, inplace=True)
 
 # ----------------------------------------------------------------------------------------------------------------------
 st.set_page_config(layout="wide")
@@ -219,7 +268,7 @@ st.markdown("""---""")
 st.header('FRIDA dataset')
 st.write('Based on the transcriptions, we are working with ', len(spks), ' speakers. In the metadata file, there are ',
          len(spks_in_meta), ' speakers with ', len(spks_in_meta_with_no_trans), ' of them having no transcriptions',
-         'Additionally, we have ', len(spks_not_in_meta), ' speakers with transcriptions with no record in the'
+         'Additionally, we have ', len(spks_not_in_meta), ' speakers with transcriptions but with no record in the'
                                                           ' metadata file.')
 
 # ---------
@@ -274,24 +323,25 @@ cols[2].dataframe(pd.DataFrame({'num of top words': np.array([10, 50, 100, 150, 
 st.header('Parameter settings')
 cols = st.columns(3)
 cols[0].write('For VOCALISE:')
-cols[0].text('recording device = telephone \n'
-             'calibrator = Logit \n'
-             'preprocessor used = MinMaxScaler')
+cols[0].text('recording device = ' + param['recording_device'] + '\n'
+             'calibrator =  ' + param['voc_calibrator'] + '\n'
+             'preprocessor used =  ' + param['voc_preprocessor'])
 
 cols[1].write('For authorship:')
-cols[1].text('num of frequent words = 200 \n'
-             'min num of words in a conversation = 1 \n'
-             'preprocessor used = percentile rank \n'
-             'distance = Bray-Curtis \n'
-             'classifier = Logistic Regression \n'
-             'calibrator = KDE')
+cols[1].text('num of frequent words = ' + str(param['num_mfw']) + '\n'
+             'min num of words in a conversation = ' + str(param['min_num_words']) + '\n'
+             'preprocessor used = ' + param['mfw_preprocessor'] + '\n'
+             'distance = ' + param['mwf_distance'] + '\n'
+             'classifier = ' + param['mwf_classifier'] + '\n'
+             'calibrator = ' + param['mwf_calibrator'])
 
 cols[2].write('General:')
-cols[2].text('max num of pairs draw per class = 2500 (for both train and test set) \n'
-             'perc of speakers used for test set = 10% (from the 223) \n'
-             'repeats = 10 \n'
-             'fusion classifier = Logistic Regression \n'
-             'fusion calibrator = KDE')
+cols[2].text('max num of pairs draw per class (train) = ' + str(param['max_pairs_per_class_train']) + '\n'
+             'max num of pairs draw per class (test) = ' + str(param['max_pairs_per_class_test']) + '\n'
+             'perc of speakers used for test set (from the 223) = ' + str(param['test_speakers_perc']) + '% \n'
+             'repeats = ' + str(param['repeats']) + '\n'
+             'fusion classifier = ' + param['fusion_classifier'] + '\n'
+             'fusion calibrator = ' + param['fusion_calibrator'] + '')
 
 st.write('We note that there are sessions with no telephone recordings. The transcriptions of those sessions were '
          'excluded from the analysis.')
@@ -340,50 +390,77 @@ cols = st.columns(5)
 option0 = cols[0].selectbox('Repeat', ('all', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'))
 option1 = cols[1].selectbox('Method A', ('mfw', 'voc', 'multi', 'comb', 'feat'))
 option2 = cols[2].selectbox('Method B', ('mfw', 'voc', 'multi', 'comb', 'feat'))
-option3 = cols[3].selectbox('Class', ('any', 'same speaker', 'different speakers'))
-option4 = cols[4].selectbox('Predictions', ('all', 'incorrect by A only', 'incorrect by B only',
-                                            'incorrect by both methods'))
+option3 = cols[3].selectbox('Class (affects only the dataframe)', ('any', 'same speaker', 'different speakers'))
+option4 = cols[4].selectbox('Predictions (affects only the dataframe)', ('all', 'incorrect by A only',
+                                                                         'incorrect by B only',
+                                                                         'incorrect by both methods'))
 
 if option0 != 'all':
-    pred_to_show = predictions[predictions['repeat'] == option0]
+    pred_subset = predictions[predictions['repeat'] == option0]
 else:
-    pred_to_show = predictions
+    pred_subset = predictions
 
 st.write(' ')
 st.write(' ')
-st.write('Confusion matrix (true labels -> rows, predicted labels -> columns):')
+st.write('Confusion matrices (true labels -> rows, predicted labels -> columns):')
 cols = st.columns(2)
-pred_A = pd.Series(np.where(pred_to_show['lrs_' + option1] > 1, 1, 0)).astype(str)
-cols[0].write('for '+option1)
-cols[0].write(confusion_matrix(pred_to_show['y'].astype(str), pred_A))
-if option1 != option2:
-    pred_B = pd.Series(np.where(pred_to_show['lrs_' + option2] > 1, 1, 0)).astype(str)
-    cols[1].write('for '+option2)
-    cols[1].write(confusion_matrix(pred_to_show['y'].astype(str), pred_B))
 
+cols[0].write('for ' + option1)
+cols[0].write(confusion_matrix(pred_subset['y'].astype(str),
+                               pd.Series(np.where(pred_subset['lrs_' + option1] > 1, 1, 0)).astype(str)))
+st.write('')
+st.write('')
+grouped_loc = accuracies_per_category(['location_A', 'location_B'], 'lrs_' + option1, 'y', pred_subset)
+grouped_env = accuracies_per_category(['environment_A', 'environment_B'], 'lrs_' + option1, 'y', pred_subset)
+grouped_2_lan = accuracies_per_category(['second_language_A', 'second_language_B'], 'lrs_' + option1, 'y', pred_subset)
+
+if option1 != option2:
+    pred_subset['pred_B'] = pd.Series(np.where(pred_subset['lrs_' + option2] > 1, 1, 0))
+    cols[1].write('for '+option2)
+    cols[1].write(confusion_matrix(pred_subset['y'].astype(str), pred_subset['pred_B'].astype(str)))
+
+    temp_loc = accuracies_per_category(['location_A', 'location_B'], 'lrs_' + option2, 'y', pred_subset)
+    grouped_loc = grouped_loc.merge(temp_loc, on=['location_A', 'location_B', 'total_0', 'total_1'])
+
+    temp_env = accuracies_per_category(['environment_A', 'environment_B'], 'lrs_' + option2, 'y', pred_subset)
+    grouped_env = grouped_env.merge(temp_env, on=['environment_A', 'environment_B', 'total_0', 'total_1'])
+
+    temp_2_lan = accuracies_per_category(['second_language_A', 'second_language_B'], 'lrs_' + option2, 'y', pred_subset)
+    grouped_2_lan = grouped_2_lan.merge(temp_2_lan, on=['second_language_A', 'second_language_B', 'total_0', 'total_1'])
+
+st.write('accuracies depending location')
+st.write(grouped_loc)
+st.write(' ')
+st.write(' ')
+st.write('accuracies depending environment')
+st.write(grouped_env)
+st.write(' ')
+st.write(' ')
+st.write('accuracies depending second language')
+st.write(grouped_2_lan)
 
 if option3 == 'same speaker':
-    pred_to_show = pred_to_show[pred_to_show['y'] == 1]
+    pred_to_show = pred_subset[pred_subset['y'] == 1]
 elif option3 == 'different speakers':
-    pred_to_show = pred_to_show[pred_to_show['y'] == 0]
-
+    pred_to_show = pred_subset[pred_subset['y'] == 0]
+else:
+    pred_to_show = pred_subset
 
 if option4 == 'incorrect by A only':
     pred_to_show = pred_to_show[(((pred_to_show['lrs_' + option1] > 1) & (pred_to_show['y'] != 1)) |
-                                ((pred_to_show['lrs_' + option1] <= 1) & (pred_to_show['y'] != 0))) &
+                                 ((pred_to_show['lrs_' + option1] <= 1) & (pred_to_show['y'] != 0))) &
                                 (((pred_to_show['lrs_' + option2] > 1) & (pred_to_show['y'] == 1)) |
-                                ((pred_to_show['lrs_' + option2] <= 1) & (pred_to_show['y'] == 0)))]
+                                 ((pred_to_show['lrs_' + option2] <= 1) & (pred_to_show['y'] == 0)))]
 elif option4 == 'incorrect by B only':
     pred_to_show = pred_to_show[(((pred_to_show['lrs_' + option1] > 1) & (pred_to_show['y'] == 1)) |
-                                ((pred_to_show['lrs_' + option1] <= 1) & (pred_to_show['y'] == 0))) &
+                                 ((pred_to_show['lrs_' + option1] <= 1) & (pred_to_show['y'] == 0))) &
                                 (((pred_to_show['lrs_' + option2] > 1) & (pred_to_show['y'] != 1)) |
-                                ((pred_to_show['lrs_' + option2] <= 1) & (pred_to_show['y'] != 0)))]
+                                 ((pred_to_show['lrs_' + option2] <= 1) & (pred_to_show['y'] != 0)))]
 elif option4 == 'incorrect by both methods':
     pred_to_show = pred_to_show[(((pred_to_show['lrs_' + option1] > 1) & (pred_to_show['y'] != 1)) |
-                                ((pred_to_show['lrs_' + option1] <= 1) & (pred_to_show['y'] != 0))) &
+                                 ((pred_to_show['lrs_' + option1] <= 1) & (pred_to_show['y'] != 0))) &
                                 (((pred_to_show['lrs_' + option2] > 1) & (pred_to_show['y'] != 1)) |
-                                ((pred_to_show['lrs_' + option2] <= 1) & (pred_to_show['y'] != 0)))]
-
+                                 ((pred_to_show['lrs_' + option2] <= 1) & (pred_to_show['y'] != 0)))]
 
 st.write(' ')
 st.write(' ')
