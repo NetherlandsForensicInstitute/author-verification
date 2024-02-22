a8cfa9fd343a26d81927755a75ac70ac1ee7302c
Sergidou Eleni Konstantina <xsergie00@pcfapso.fit.vutbr.cz>
2024-02-22 10:51:52 +0100

    further changes to work with fisher

--- authorship/transcriptions.py
+++ authorship/transcriptions.py
@@ -51,7 +51,7 @@ class DataSource:
             speakers_wordlist = load_data(speakers_path)
         else:
             speakers_wordlist = compile_data(self._data_name, self._data_path, self._ground_truth, self._extra_info,
-                                             self._remove_filler_sounds)
+                                             self._remove_filler_sounds, self._expand_contractions)
             store_data(speakers_path, speakers_wordlist)
 
         # extract a list of frequent words
@@ -61,7 +61,7 @@ class DataSource:
         speakers_conv = filter_texts(speakers_wordlist, wordlist, self._min_num_of_words)
 
         # convert to X, y (= spk id) but keep the conversation id
-        X, y, conv_ids = to_vector_size(speakers_conv)
+        X, y, conv_ids = to_vector_size(speakers_conv, self._data_name)
 
         return X, y, conv_ids
 
@@ -105,6 +105,7 @@ def read_session(lines, data_name, ground_truth, remove_filler_words=False, expa
         :param data_name: name/id of the dataset (e.g., frida or fisher)
         :param ground_truth: whether the input are manual transcriptions (then True) or ASR output (then False)
         :param remove_filler_words: whether filler sounds such as uh, uh huh, ehm to be removed or not
+        :param expand_contractions:
     """
     lines_to_words = lines.read()
     words_to_remove = []
@@ -124,7 +125,7 @@ def read_session(lines, data_name, ground_truth, remove_filler_words=False, expa
 
             lines_to_words = (lines_to_words.replace('start\tend\ttext\n', '').replace('-', ' ')
                               .replace('\n', ' ').replace('*v', '').replace('*s', '')
-                              .replace('*n', '').replace('*u', ''))
+                              .replace('*n', ''))
 
             lines_to_words = re.sub('[A-Za-z]*\*', '', lines_to_words)  # for words with missing notation
 
@@ -141,20 +142,47 @@ def read_session(lines, data_name, ground_truth, remove_filler_words=False, expa
                 words_to_remove.extend(['a', 'e', 'oo', 'oe', 'aa', 'ee', 'mm', 'he', 'eh', 'uh', 'hm', 'jo',
                                         'uhu', 'uhm', 'eeh', 'nah', 'joo', 'ach', 'hoo', 'woo'])
 
+        elif data_name == 'fisher':
+            lines_to_words = re.sub('[0-9]*\.[0-9]*\ [0-9]*\.[0-9]*\ [AB]:', '', lines_to_words)
+            lines_to_words = re.sub('\[\[[a-zA-Z]*\]\]', '', lines_to_words)  # -> <SKIP>
+            lines_to_words = re.sub('\[[a-zA-Z]*\]', '', lines_to_words)  # -> <SOUND>
+            lines_to_words = re.sub('\(\(', '', lines_to_words)  # remove (( and )) but keep text inside
+            lines_to_words = re.sub('\)\)', '', lines_to_words)
+            lines_to_words = re.sub('[a-zA-Z]*- ', ' ', lines_to_words)
+            lines_to_words = lines_to_words.replace('\n', ' ')
+            lines_to_words = lines_to_words.replace('-', ' ')  # so uh-huh is uh huh, in this point because
+            # words like a three-year-old and
+            # son-in-law are transcribed with -
+            # between the words
+
+            if remove_filler_words:
+                words_to_remove.extend(['er', 'eh', 'ah', 'hm', 'ha', 'um', 'em', 'uh', 'oh', 'mm', 'ya', 'hmm', 'ohh',
+                                        'huh', 'gee', 'mhm', 'nah', 'ahem', 'jeeze'])
+
+            if expand_contractions:
+                lines_to_words = jiwer.ExpandCommonEnglishContractions()(lines_to_words)
+
     elif not ground_truth:
         if data_name == 'frida':
-            lines_to_words = lines_to_words.replace('Eén', 'één')
+            lines_to_words = (lines_to_words.replace('Eén', 'één').replace('ok', 'oké').replace('oke', 'oké')
+                              .replace('haha', '').replace('hahaha', ''))
 
             if expand_contractions:
                 # only treat the ones I saw
                 lines_to_words = (lines_to_words.replace('z\'n', 'zijn').replace('\zo\'n', 'zo een')
-                                  .replace('m\'r', 'maar').replace('ok', 'oké').replace('oke', 'oké'))
+                                  .replace('m\'r', 'maar'))
 
             if remove_filler_words:
                 words_to_remove.extend(['a', 'e', 'o', 'z', 'he', 'hé', 'hè', 'uh', 'oh', 'ah', 'eh', 'ha', 'hm', 'jo',
                                         'yo', 'ach', 'huh', 'hmm', 'ehh', 'ehm', 'uhm', 'eee', 'joh', 'oei', 'oeh',
-                                        'haha', 'hahaha', 'mmhmm'])
+                                        'mmhmm'])
+
         elif data_name == 'fisher':
+            lines_to_words = lines_to_words.replace('ok', 'okay')
+
+            if remove_filler_words:
+                words_to_remove.extend(['oh', 'um', 'uh', 'mm', 'ah', 'ha', 'huh', 'hmm', 'uhhuh', 'mmhmm'])
+
             if expand_contractions:
                 lines_to_words = jiwer.ExpandCommonEnglishContractions()(lines_to_words)
 
@@ -176,16 +204,25 @@ def compile_data(data_name, index_path, ground_truth, info_path=None, remove_fil
     """
     it takes a path to folder. It loops through all the .txt files in the folder and returns a dictionary
     where the key is the id of a conversation and the value a list of texts
+        :param data_name:
         :param index_path: str
+        :param ground_truth:
+        :param info_path:
+        :param remove_filler_words:
+        :param expand_contractions:
     """
     to_ids = {}
     if data_name == 'fisher' and info_path is not None:
         with open(info_path) as f:
             next(f)
             for line in f:
-                # each line has the filename, id of speaker, transcriber (LDC or BBN)
-                filename, spk_id, trans = line.split('\t', 2)
-                to_ids[filename] = [spk_id, trans.replace('\n', '')]
+                # each line has the filename, id of speaker, transcriber (LDC or BBN), sex of the speaker,
+                # whether the speaker is native english or not, and how many conversations the speaker had
+                # (see update_info_file.py or folder fisher_metadata)
+                filename_full, spk_id, trans, sx, dl, count = line.split('\t', 5)
+                filename = (filename_full.replace('fe_03_', '').replace('.txt', '').replace('_a', 'a')
+                            .replace('-a', 'a').replace('_b', 'b').replace('-b', 'b'))
+                to_ids[filename] = [spk_id, trans, sx, count.replace('\n', '')]
 
     conversations = collections.defaultdict(list)  # create empty dictionary list
     files = glob.glob(os.path.join(index_path, "*.txt"))
@@ -204,9 +241,11 @@ def compile_data(data_name, index_path, ground_truth, info_path=None, remove_fil
 
         elif data_name == 'fisher':
             # from fe_03_XXXXX{-_}{ab}.txt to XXXXX{ab} (for ref it is '-' and for asr is '_')
-            conv_id = (path_str.replace('fe_03_', '').replace('.txt', '').replace('_a', 'a')
-                       .replace('-a', 'a').replace('_b', 'b').replace('-b', 'b'))
-            file_to_check = (path_str.replace('.txt', '').replace('_a', '-a').replace('_b', '-b'))
+            file_to_check = (path_str.replace('fe_03_', '').replace('.txt', '')
+                             .replace('_a', 'a').replace('-a', 'a')
+                             .replace('_b', 'b').replace('-b', 'b'))
+
+            conv_id = to_ids.get(file_to_check)[0] + '_' + file_to_check
         else:
             conv_id = 'error'
             print('unexpected data name')
@@ -214,17 +253,16 @@ def compile_data(data_name, index_path, ground_truth, info_path=None, remove_fil
 
         with open(filepath, "r") as f:
 
-            if data_name == 'fisher' and info_path is not None:
-                if to_ids.get(file_to_check)[1] == 'LDC':
-                    continue
-
-            text = read_session(f, data_name, ground_truth, remove_filler_words, expand_contractions)
-            if len(text) > 0:  # to exclude empty files
-                conversations[conv_id] = text
-            # if 'ewa' in text:
-            #     print(conv_id)
-            # else:
-            #     print('empty file = ' + conv_id)
+            if data_name == 'fisher' and (to_ids.get(file_to_check)[2] == '' or to_ids.get(file_to_check)[3] == '1'):
+                continue
+            else:
+                text = read_session(f, data_name, ground_truth, remove_filler_words, expand_contractions)
+                if len(text) > 0:  # to exclude empty files
+                    conversations[conv_id] = text
+                # if 'ewa' in text:
+                #     print(conv_id)
+                # else:
+                #     print('empty file = ' + conv_id)
 
     return conversations
 
@@ -253,6 +291,7 @@ def filter_texts(conversations, wordlist, min_num_of_words):
 
     :param conversations: dict of all words used per conversation
     :param wordlist: the n most freq words in corpus
+    :param min_num_of_words:
     """
     f = CreateFeatureVector(wordlist)
 
@@ -262,12 +301,12 @@ def filter_texts(conversations, wordlist, min_num_of_words):
         ltext = len(text)
         filtered_text = list(f(text))
         if ltext > min_num_of_words:
-            filtered[label] = [i/ltext for i in filtered_text]
+            filtered[label] = [i / ltext for i in filtered_text]
 
     return filtered
 
 
-def to_vector_size(speakers):
+def to_vector_size(speakers, data_name):
     """
     returns a matrix where each row correspond to a conversation by a speaker, a vector that holds the id of the
     speaker, and a vector that holds the id of the conversation
@@ -280,7 +319,10 @@ def to_vector_size(speakers):
 
     for conv_id, texts in speakers.items():
         features.append(texts)
-        speaker_ids.append(conv_id[2:(len(conv_id) - 2)])
+        if data_name == 'frida':
+            speaker_ids.append(conv_id[2:(len(conv_id) - 2)])
+        elif data_name == 'fisher':
+            speaker_ids.append(conv_id.split('_')[0])
         conv_ids.append(conv_id)
 
-    return np.concatenate(features),  np.array(speaker_ids),  np.array(conv_ids)
+    return np.concatenate(features), np.array(speaker_ids), np.array(conv_ids)
---
+++ update_info_file.py
@@ -0,0 +1,74 @@
+#
+# bringing metadata in the info file
+#
+import pandas as pd
+import numpy as np
+
+#  column name -- Description
+# 0  CALLID    -- 5-digit number, matches file names (fe_03_CALLID.*)
+# 1  DATE_TIME -- when the call was recorded: YYYYMMDD_HH24:MI:SS
+# 2  TOPICID   -- ENGnn, matches the "id=..." attribute of a topic in topics.sgm
+# 3  SIG_GRADE -- variable-length numeric ("N" or "N.N"): perceived signal quality
+# 4  CNV_GRADE -- variable-length numeric ("N" or "N.N"): perceived conversation quality
+# 5  APIN      -- subject-id of speaker on channel A, matches first field of pindata.tbl
+# 6  ASX.DL    -- [m|f].[a.o] : male or female, American or other English dialect
+# 7  APHNUM    -- alphanumeric, "encrypted" phone number of caller A (if known)
+# 8  APHSET    -- phone-set type: 1=speaker-phone, 2=headset, 3=ear-bud, 4=hand-held
+# 9  APHTYP    -- phone-service type: 1=cell, 2=cordless, 3=regular (land-line)
+# 10 BPIN      -- channel B data, same as for channel A
+# 11 BSX.DL
+# 12 BPHNUM
+# 13 BPHSET
+# 14 BPHTYP
+
+# load data: each file contains info for approximately half of the conversations
+# each row corresponds to a call
+path1 = 'fisher/fisher_metadata/fe_03_p1_calldata.tbl'
+path2 = 'fisher/fisher_metadata/fe_03_p2_calldata.tbl'
+
+meta1 = pd.read_table(path1, sep=',', usecols=[0, 5, 6, 10, 11], dtype={'CALL_ID': str, 'APIN': str, 'BPIN': str})
+meta2 = pd.read_table(path2, sep=',', usecols=[0, 5, 6, 10, 11], dtype={'CALL_ID': str, 'APIN': str, 'BPIN': str})
+
+# from conversations to speaker/conversation
+metaA = pd.concat([meta1.iloc[:, [0, 1, 2]], meta2.iloc[:, [0, 1, 2]]], ignore_index=True)
+metaA.columns = ['call_id', 'spk_id', 'sx.dl']
+metaB = pd.concat([meta1.iloc[:, [0, 3, 4]], meta2.iloc[:, [0, 3, 4]]], ignore_index=True)
+metaB.columns = ['call_id', 'spk_id', 'sx.dl']
+
+meta = pd.concat([metaA, metaB], ignore_index=True)
+meta[['sx', 'dl']] = meta['sx.dl'].str.split(".", expand=True, )
+meta.drop('sx.dl', axis=1, inplace=True)
+
+# identify speakers with consistent metadata in terms of sex and language
+spk_all = meta[['spk_id', 'sx', 'dl']].drop_duplicates()
+spk_keep = spk_all.drop_duplicates(subset=['spk_id'], keep=False)
+
+# those speakers with messy metadata
+spk_mess = meta[~meta.spk_id.isin(spk_keep.spk_id)]
+
+# per speaker with messy metadata find mode (the value that appears most often, there could be multiple ones)
+spk_mess_to_clean = spk_mess.groupby(['spk_id'])[['sx', 'dl']].agg(pd.Series.mode)
+spk_mess_to_clean.reset_index(inplace=True)
+
+spk_mess_to_clean['sx_unclear'] = spk_mess_to_clean.apply(lambda x: True if len(x['sx']) > 1 else False, axis=1)
+spk_mess_to_clean['dl_unclear'] = spk_mess_to_clean.apply(lambda x: True if len(x['dl']) > 1 else False, axis=1)
+spk_mess_to_clean.rename(columns={"sx": "sx_temp", "dl": "dl_temp"}, inplace=True)
+
+meta = meta.merge(spk_mess_to_clean, on='spk_id', how='left')
+meta['sx'] = np.where(meta.sx_temp.isnull(), meta.sx, np.where(meta.sx_unclear==True, np.nan, meta.sx_temp))
+meta['dl'] = np.where((meta.dl_temp.isnull()) & (meta.dl!='u'), meta.dl,
+                      np.where((meta.dl_unclear==True) | (meta.dl=='u'), np.nan, meta.dl_temp))
+
+info_path = '/mnt/matylda3/xsergie00/pycharm/PycharmProjects/startingwithwhisper/data/audio/fisher/info.txt'
+info_df = pd.read_csv(info_path, sep="\t", header=None, skiprows=1)
+info_df.columns = ['file_name', 'spk_id', 'transcriber_id']
+info_df.file_name = info_df.file_name.str.replace('-', '_')
+info_df.spk_id = info_df.spk_id.str.replace('FISHE', '')
+info_df.transcriber_id = info_df.transcriber_id.str.replace('/WordWave', '')
+info_df['call_id'] = info_df.file_name.str.replace('fe_03_', '').str.replace('_a', '').str.replace('_b', '')
+
+info_extra = pd.merge(info_df, meta, how='left', on=['call_id', 'spk_id'])
+info_extra.drop(['call_id', 'sx_temp', 'dl_temp', 'sx_unclear', 'dl_unclear'], axis=1, inplace=True)
+info_extra['count'] = info_extra.groupby('spk_id')['spk_id'].transform('size')
+
+info_extra.to_csv('fisher/info_extra.txt', index=None, sep='\t')
